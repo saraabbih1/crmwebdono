45870de12def4c88c0887eb25edd10ecfad4af68
saraabbih1 <abbihsara30@gmail.com>
2026-05-07 21:38:40 +0100

    feat: migration des tableau client,subscription,notification

---
+++ database/migrations/2026_05_07_203334_create_subscriptions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+
+            $table->string('service_type');
+
+            $table->date('start_date');
+            $table->date('end_date');
+
+            $table->string('status');
+
+            $table->decimal('price', 8, 2)->nullable();
+
+            $table->string('payment_status')->default('unpaid');
+
+            $table->text('message_reminder')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
