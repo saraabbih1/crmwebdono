e3a84225ed01de45989e4e62ac43e497236c3f04
saraabbih1 <abbihsara30@gmail.com>
2026-05-07 22:45:25 +0100

    feat: routers

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\SubscriptionController;
+
 Route::get('/', function () {
     return view('welcome');
-    Route::resource('clients', ClientController::class);
 });
+
+Route::resource('clients', ClientController::class);
+Route::resource('subscriptions', SubscriptionController::class)->except(['show']);
+Route::resource('notifications', NotificationController::class)->except(['show']);
