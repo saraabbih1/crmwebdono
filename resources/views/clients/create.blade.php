234944dc365cc01ed138ae67617faffad40d29b5
saraabbih1 <abbihsara30@gmail.com>
2026-05-13 16:34:02 +0100

    Update resources/views/clients/create.blade.php

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -6,13 +6,17 @@
 
 @section('content')
     <div class="card content-card">
+        <div class="card-header bg-transparent border-0 pt-4 px-4">
+            <div class="fw-semibold">Client profile</div>
+            <div class="small text-secondary">Add contact details used by subscriptions and reminders.</div>
+        </div>
         <div class="card-body">
             <form action="{{ route('clients.store') }}" method="POST" class="row g-3">
                 @csrf
                 @include('clients.form', ['client' => null])
-                <div class="col-12">
+                <div class="col-12 d-flex gap-2 pt-2">
                     <button class="btn btn-primary">Save client</button>
-                    <a href="{{ route('clients.index') }}" class="btn btn-link">Cancel</a>
+                    <a href="{{ route('clients.index') }}" class="btn btn-outline-secondary">Cancel</a>
                 </div>
             </form>
         </div>
