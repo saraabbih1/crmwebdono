7d4e231499fee2680565e62feca79a4eab9d9080
saraabbih1 <abbihsara30@gmail.com>
2026-05-13 16:34:04 +0100

    Update resources/views/subscriptions/create.blade.php

--- resources/views/subscriptions/create.blade.php
+++ resources/views/subscriptions/create.blade.php
@@ -6,13 +6,17 @@
 
 @section('content')
     <div class="card content-card">
+        <div class="card-header bg-transparent border-0 pt-4 px-4">
+            <div class="fw-semibold">Subscription setup</div>
+            <div class="small text-secondary">Renewal and reminder dates are calculated from the service type.</div>
+        </div>
         <div class="card-body">
             <form action="{{ route('subscriptions.store') }}" method="POST" class="row g-3">
                 @csrf
                 @include('subscriptions.form', ['subscription' => null])
-                <div class="col-12">
+                <div class="col-12 d-flex gap-2 pt-2">
                     <button class="btn btn-primary">Save subscription</button>
-                    <a href="{{ route('subscriptions.index') }}" class="btn btn-link">Cancel</a>
+                    <a href="{{ route('subscriptions.index') }}" class="btn btn-outline-secondary">Cancel</a>
                 </div>
             </form>
         </div>
