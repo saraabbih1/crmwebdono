0ddadaeb41d93f3c102b445853eb73f6a9619d4b
saraabbih1 <abbihsara30@gmail.com>
2026-05-13 16:34:03 +0100

    Update resources/views/notifications/create.blade.php

--- resources/views/notifications/create.blade.php
+++ resources/views/notifications/create.blade.php
@@ -6,13 +6,17 @@
 
 @section('content')
     <div class="card content-card">
+        <div class="card-header bg-transparent border-0 pt-4 px-4">
+            <div class="fw-semibold">Manual email notification</div>
+            <div class="small text-secondary">Pending notifications are sent by the scheduled reminder command.</div>
+        </div>
         <div class="card-body">
             <form action="{{ route('notifications.store') }}" method="POST" class="row g-3">
                 @csrf
                 @include('notifications.form', ['notification' => null])
-                <div class="col-12">
+                <div class="col-12 d-flex gap-2 pt-2">
                     <button class="btn btn-primary">Save notification</button>
-                    <a href="{{ route('notifications.index') }}" class="btn btn-link">Cancel</a>
+                    <a href="{{ route('notifications.index') }}" class="btn btn-outline-secondary">Cancel</a>
                 </div>
             </form>
         </div>
