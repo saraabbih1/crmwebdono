c911526a77ec844dcc49635025d23e042cdc8f62
saraabbih1 <abbihsara30@gmail.com>
2026-05-13 16:34:03 +0100

    Update resources/views/notifications/edit.blade.php

--- resources/views/notifications/edit.blade.php
+++ resources/views/notifications/edit.blade.php
@@ -6,14 +6,18 @@
 
 @section('content')
     <div class="card content-card">
+        <div class="card-header bg-transparent border-0 pt-4 px-4">
+            <div class="fw-semibold">Manual email notification</div>
+            <div class="small text-secondary">Review status, reminder date, and delivery timing.</div>
+        </div>
         <div class="card-body">
             <form action="{{ route('notifications.update', $notification) }}" method="POST" class="row g-3">
                 @csrf
                 @method('PUT')
                 @include('notifications.form')
-                <div class="col-12">
+                <div class="col-12 d-flex gap-2 pt-2">
                     <button class="btn btn-primary">Update notification</button>
-                    <a href="{{ route('notifications.index') }}" class="btn btn-link">Cancel</a>
+                    <a href="{{ route('notifications.index') }}" class="btn btn-outline-secondary">Cancel</a>
                 </div>
             </form>
         </div>
