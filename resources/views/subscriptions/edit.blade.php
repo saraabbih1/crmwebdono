a258b7195a71754eb6bf22a4bebeefdfba902274
saraabbih1 <abbihsara30@gmail.com>
2026-05-13 16:34:04 +0100

    Update resources/views/subscriptions/edit.blade.php

--- resources/views/subscriptions/edit.blade.php
+++ resources/views/subscriptions/edit.blade.php
@@ -6,14 +6,18 @@
 
 @section('content')
     <div class="card content-card">
+        <div class="card-header bg-transparent border-0 pt-4 px-4">
+            <div class="fw-semibold">Subscription setup</div>
+            <div class="small text-secondary">Changes will refresh the pending reminder notification.</div>
+        </div>
         <div class="card-body">
             <form action="{{ route('subscriptions.update', $subscription) }}" method="POST" class="row g-3">
                 @csrf
                 @method('PUT')
                 @include('subscriptions.form')
-                <div class="col-12">
+                <div class="col-12 d-flex gap-2 pt-2">
                     <button class="btn btn-primary">Update subscription</button>
-                    <a href="{{ route('subscriptions.index') }}" class="btn btn-link">Cancel</a>
+                    <a href="{{ route('subscriptions.index') }}" class="btn btn-outline-secondary">Cancel</a>
                 </div>
             </form>
         </div>
